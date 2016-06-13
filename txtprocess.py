ef87214648f7465ce61f71a598fc68ac56a98268
Blake Boswell <j.blake.boswell@gmail.com>
2016-06-12 23:43:58 -0400

    updated notebook to include cosine estimator and transform

--- txtprocess.py
+++ txtprocess.py
@@ -44,31 +44,10 @@ class KeySelect(TransformerMixin):
         return self
 
 
-class FileIterKeySelect(TransformerMixin):
-    ''' select key from dictionary
-    '''
-    def __init__(self, xkey='content', ykey='label'):
-        self.xkey = xkey
-        self.ykey = ykey
-
-    def _json_load(self, line):
-        return json.loads(line)
-
-    def _select_keys(self, element):
-        return element[self.xkey], element[self.ykey]
-
-    def transform(self, f, **transform_params):
-        x, y = zip(*[self._select_keys(self._json_load(line)) for line in f])
-        yield x, y
-
-    def fit(self, X, y=None, **fit_params):
-        return self
-
-
 class StripTransform(TransformerMixin):
     ''' transform raw content to only word chars
     '''
-    def transform(self, X, **transform_params):
+    def transform(self, X, y=None, **transform_params):
         return [_strip_stops(_strip_numbers(_strip_punct(x)))
                  for x in X]
 
@@ -82,8 +61,8 @@ class CosineSim(BaseEstimator):
     def transform(self, X, **transform_params):
         return self
 
-    def fit(self, X, y=None, **fit_params):
-        indices = np.array([e for e in y if e == 2]).nonzero()[0]
+    def fit(self, X, y, **fit_params):
+        indices = np.array([i for i in range(len(y)) if y[i] == 1])
         self.X = X[indices, :]
 
     def predict(self, X, y=None, **predict_params):
@@ -91,4 +70,17 @@ class CosineSim(BaseEstimator):
         d = d / d.sum(axis=1)[:, np.newaxis] #normalize
         return d.sum(axis=0)
 
+
+class CosineSimTrans(TransformerMixin):
+    ''' store vector of target, predict by calculating cosine distance
+    '''
+    def transform(self, X, **transform_params):
+        d = pairwise_distances(self.X, X, metric='cosine')
+        d = d / d.sum(axis=1)[:, np.newaxis] #normalize
+        return d.sum(axis=0).reshape(-1, 1)
+
+    def fit(self, X, y, **fit_params):
+        indices = np.array([i for i in range(len(y)) if y[i] == 1])
+        self.X = X[indices, :]
+        return self
 
